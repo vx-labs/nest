a46c15c332b6c4deab47cb9e29b5bc5a3bef475b
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-06-18 19:05:28 +0200

    rename nest proto file

--- nest/api/types.go
+++ nest/api/types.go
@@ -1,3 +1,3 @@
 package api
 
-//go:generate protoc -I ${GOPATH}/src/github.com/vx-labs/nest/vendor -I ${GOPATH}/src/github.com/vx-labs/nest/vendor/github.com/gogo/protobuf/ -I ${GOPATH}/src/github.com/vx-labs/nest/nest/api api.proto --go_out=plugins=grpc:.
+//go:generate protoc -I ${GOPATH}/src/github.com/vx-labs/nest/vendor -I ${GOPATH}/src/github.com/vx-labs/nest/vendor/github.com/gogo/protobuf/ -I ${GOPATH}/src/github.com/vx-labs/nest/nest/api nest.proto --go_out=plugins=grpc:.
