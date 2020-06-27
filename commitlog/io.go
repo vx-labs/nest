bb156b80a3ce2d2c512df36ff17ad39fae3b1c41
Julien BONACHERA <julien.bonachera@fortuneo.com>
2020-06-27 23:53:27 +0200

    commitlog: remove readerat

--- commitlog/io.go
+++ commitlog/io.go
@@ -8,15 +8,3 @@ type ReadSeekCloser interface {
 	io.ReadCloser
 	io.Seeker
 }
-
-type readerAt struct {
-	pos uint64
-	r   io.ReaderAt
-}
-
-func (r *readerAt) Read(buf []byte) (int, error) {
-	n, err := r.r.ReadAt(buf, int64(r.pos))
-	r.pos += uint64(n)
-
-	return n, err
-}
